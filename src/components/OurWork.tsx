f073a98643f658dedf0922ac4eeb2cb27e35737c
saim <saimmufeez123@gmail.com>
2025-03-07 12:28:10 -0800

    Responsiveness of Our Work section image done.

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -4,7 +4,8 @@ const OurWork = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row md:gap-[60px] gap-10 md:px-5 px-3 2xl:mx-auto py-16 md:justify-between">
-        <div className="text-[#4D4D4D] flex flex-col  md:w-[60%] md:h-[50vh] lg:px-0 lg:w-[70%]">
+        {/* Text Section */}
+        <div className="text-[#4D4D4D] flex flex-col md:w-[60%] md:h-[50vh] lg:px-0 lg:w-[70%]">
           <div className="flex flex-col gap-4 md:gap-8">
             <h2 className="text-[26px] sm:text-3xl 2xl:text-[40px] text-center font-bold font-serif text-[#4D4D4D]">
               Who are we
@@ -24,8 +25,13 @@ const OurWork = () => {
           </div>
         </div>
 
-        <div className="w-full md:w-[40%] lg:w-[50%] 2xl:w-auto">
-          <img src={OurWorkImage} className="w-full h-[70vh] rounded-lg "></img>
+        {/* Image Section */}
+        <div className="w-full md:w-2/5 lg:w-1/3">
+          <img
+            src={OurWorkImage}
+            className="w-full h-auto max-h-[70vh] rounded-lg object-cover"
+            alt="Our Work"
+          />
         </div>
       </div>
     </>
